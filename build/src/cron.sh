f45d391ecd454c27dc73856ddc911bdb9553d665
Jon Goodger <jonno.is@gmail.com>
2023-03-29 10:20:12 +0100

    Add source

---
+++ build/src/cron.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# TODO: Check if this command is already running
+
+echo Running WP Cron...
+echo WordPress Host: $WORDPRESS_HOST
+exec wget -q -O - http://${WORDPRESS_HOST:-web}/wp-cron.php?doing_wp_cron 2>&1
